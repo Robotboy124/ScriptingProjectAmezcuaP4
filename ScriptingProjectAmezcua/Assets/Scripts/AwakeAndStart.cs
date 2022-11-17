309864556fb797a55245e38ffd89507e6aa1c16d
Robotboy124 <robotboy1067@gmail.com>
2022-11-17 10:32:52 -0800

    Awake and start script added
    Added script and scene showcasing the difference between awake and start

---
+++ ScriptingProjectAmezcua/Assets/Scripts/AwakeAndStart.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AwakeAndStart : MonoBehaviour
+{
+    void Awake()
+    {
+        Debug.Log("Awake has been called.");
+    }
+
+    void Start()
+    {
+        Debug.Log("Start has been called.");
+    }
+}
